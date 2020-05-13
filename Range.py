554c53ecbf3b8bc6c78e3dc1cc9183f927c80643
OilerGera <65278224+OilerGera@users.noreply.github.com>
2020-05-13 20:55:31 +0200

    Create Range.py

---
+++ Range.py
@@ -0,0 +1,19 @@
+# a = [[5,6], [-3,100], [9,0]]
+# 
+# 
+# # for i in range ( len(a)):
+# #     for j in range (len(a[i])):
+# 
+# for row in a:
+#     for elem in row:
+#         print ("{},{} = {}", end = " ")
+
+
+a = [[1,2,3,4], [5,6], [7,8,9]]
+
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        sum (a[0] + a[1] + a[2])
+        break
+        
+    print(sum)
